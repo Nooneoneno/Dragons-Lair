a646dfe1393e3382cd5687436906ba9c215c73e5
Cosmo Kevin Antenucci <kevin>
2024-07-09 11:25:24 +0200

    Created videogame class

---
+++ lib/entities/video_game.dart
@@ -0,0 +1,17 @@
+class VideoGame {
+  final String name;
+  final String genre;
+  final String online;
+  final String publisher;
+  final String referenceStore;
+  final DateTime releaseDate;
+
+  VideoGame({
+    required this.name,
+    required this.genre,
+    required this.online,
+    required this.publisher,
+    required this.referenceStore,
+    required this.releaseDate,
+  });
+}
