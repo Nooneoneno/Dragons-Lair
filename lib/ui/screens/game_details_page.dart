e6485e8e5c1348a73a0dc5da80bcb013a2c01efe
kevin <cosmoantenucci@gmail.com>
2024-08-28 12:03:37 +0200

    Added game details page and the gesture detector to game card widget to open the game details page

---
+++ lib/ui/screens/game_details_page.dart
@@ -0,0 +1,139 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
+import 'package:progetto_esame/entities/video_game.dart';
+
+class GameDetailsPage extends StatelessWidget {
+  final VideoGame game;
+
+  GameDetailsPage({required this.game});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(children: [
+      game.coverUrl != null && game.coverUrl.isNotEmpty
+          ? Image.network(
+              game.coverUrl,
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            )
+          : Image.asset(
+              'assets/placeholder.jpg',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            ),
+      Positioned.fill(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+          child: Container(
+            color:
+                Colors.black.withOpacity(0),
+          ),
+        ),
+      ),
+      SafeArea(
+        child: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.favorite_border_outlined, color: Colors.white),
+              onPressed: () {
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.playlist_add, color: Colors.white),
+              onPressed: () {
+                Fluttertoast.showToast(
+                  msg: "Game added to your library!",
+                  toastLength: Toast.LENGTH_LONG,
+                  gravity: ToastGravity.TOP,
+                  backgroundColor: Colors.white,
+                  textColor: Colors.black87,
+                  fontSize: 16.0,
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      DraggableScrollableSheet(
+        initialChildSize: 0.33,
+        minChildSize: 0.33,
+        maxChildSize: 0.92,
+        builder: (BuildContext context, ScrollController scrollController) {
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.8),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              ),
+            ),
+            padding: EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              controller: scrollController,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Container(
+                      width: 40,
+                      height: 5,
+                      decoration: BoxDecoration(
+                        color: Colors.white54,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      margin: EdgeInsets.only(bottom: 16.0),
+                    ),
+                  ),
+                  Text(
+                    game.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Release Date: ${DateFormat.yMMMMd().format(game.humanFirstReleaseDate)}',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  if (game.storyline != null && game.storyline.isNotEmpty)
+                    Text(
+                      game.storyline,
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16,
+                      ),
+                    )
+                  else
+                    Text(
+                      'No description available.',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16,
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    ]));
+  }
+}
--- lib/ui/screens/home_screen/game_card.dart
+++ lib/ui/screens/home_screen/game_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:progetto_esame/entities/video_game.dart';
+import 'package:progetto_esame/ui/screens/game_details_page.dart';
 
 class GameCard extends StatelessWidget {
   final VideoGame game;
@@ -9,81 +10,90 @@ class GameCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 9 / 21,
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: game.coverUrl != null && game.coverUrl.isNotEmpty
-              ? NetworkImage(game.coverUrl)
-                : AssetImage('assets/placeholder.jpg'),
-            fit: BoxFit.fill,
-          ),
-        ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 5,
-              left: 5,
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                color: Colors.black54,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      game.humanFirstReleaseDate.day.toString(),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Column(
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => GameDetailsPage(game: game),
+            ),
+          );
+        },
+        child: AspectRatio(
+          aspectRatio: 9 / 21,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: game.coverUrl != null && game.coverUrl.isNotEmpty
+                    ? NetworkImage(game.coverUrl)
+                    : AssetImage('assets/placeholder.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 5,
+                  left: 5,
+                  child: Container(
+                    padding: EdgeInsets.all(8.0),
+                    color: Colors.black54,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Container(
-                          width: 2,
-                          color: Colors.white,
-                          margin: EdgeInsets.symmetric(horizontal: 8.0),
-                          height: 30,
+                        Text(
+                          game.humanFirstReleaseDate.day.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              width: 2,
+                              color: Colors.white,
+                              margin: EdgeInsets.symmetric(horizontal: 8.0),
+                              height: 30,
+                            ),
+                          ],
+                        ),
+                        Text(
+                          DateFormat.MMMM().format(game.humanFirstReleaseDate),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ],
                     ),
-                    Text(
-                      DateFormat.MMMM().format(game.humanFirstReleaseDate),
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    alignment: Alignment.center,
+                    color: Colors.black54,
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      game.name,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 21,
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 20,
-              left: 0,
-              right: 0,
-              child: Container(
-                alignment: Alignment.center,
-                color: Colors.black54,
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  game.name,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
