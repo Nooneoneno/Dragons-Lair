a540235daeff879c3153eac200ec6712ac0fda4c
kevin <cosmoantenucci@gmail.com>
2024-08-29 15:28:17 +0200

    Fixed video game details panel

--- lib/ui/screens/game_details_page.dart
+++ lib/ui/screens/game_details_page.dart
@@ -32,17 +32,17 @@ class GameDetailsPage extends StatelessWidget {
         body: Stack(children: [
           game.coverUrl != null && game.coverUrl.isNotEmpty
               ? Image.network(
-                  game.coverUrl,
-                  width: double.infinity,
-                  height: double.infinity,
-                  fit: BoxFit.cover,
-                )
+            game.coverUrl,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          )
               : Image.asset(
-                  'assets/placeholder.jpg',
-                  width: double.infinity,
-                  height: double.infinity,
-                  fit: BoxFit.cover,
-                ),
+            'assets/placeholder.jpg',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
           Positioned.fill(
             child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 0.2, sigmaY: 0.2),
@@ -64,90 +64,107 @@ class GameDetailsPage extends StatelessWidget {
                     topRight: Radius.circular(20.0),
                   ),
                 ),
-                padding: EdgeInsets.all(16.0),
-                child: SingleChildScrollView(
-                  controller: scrollController,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Center(
-                        child: Container(
-                          width: 40,
-                          height: 5,
-                          decoration: BoxDecoration(
-                            color: Colors.white54,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          margin: EdgeInsets.only(bottom: 16.0),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+                child: Stack(
+                  children: [
+                    SingleChildScrollView(
+                      controller: scrollController,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Expanded(
-                            child: Text(
-                              game.name,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
+                          SizedBox(
+                            height: 25.0,
                           ),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              IconButton(
-                                icon: Icon(Icons.favorite_border_outlined,
-                                    color: Colors.white),
-                                onPressed: () {
-                                },
+                              Expanded(
+                                child: Text(
+                                  game.name,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ),
-                              IconButton(
-                                icon: Icon(Icons.playlist_add,
-                                    color: Colors.white),
-                                onPressed: () {
-                                  Fluttertoast.showToast(
-                                    msg: "Game added to your library!",
-                                    toastLength: Toast.LENGTH_LONG,
-                                    gravity: ToastGravity.TOP,
-                                    backgroundColor: Colors.white,
-                                    textColor: Colors.black87,
-                                    fontSize: 16.0,
-                                  );
-                                },
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.favorite_border_outlined,
+                                        color: Colors.white),
+                                    onPressed: () {},
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.playlist_add,
+                                        color: Colors.white),
+                                    onPressed: () {
+                                      Fluttertoast.showToast(
+                                        msg: "Game added to your library!",
+                                        toastLength: Toast.LENGTH_LONG,
+                                        gravity: ToastGravity.TOP,
+                                        backgroundColor: Colors.white,
+                                        textColor: Colors.black87,
+                                        fontSize: 16.0,
+                                      );
+                                    },
+                                  ),
+                                ],
                               ),
                             ],
                           ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Release Date: ${DateFormat.yMMMMd().format(game.humanFirstReleaseDate)}',
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 18,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          if (game.storyline != null &&
+                              game.storyline.isNotEmpty)
+                            Text(
+                              game.storyline,
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 16,
+                              ),
+                            )
+                          else
+                            Text(
+                              'No description available.',
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 16,
+                              ),
+                            ),
                         ],
                       ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Release Date: ${DateFormat.yMMMMd().format(game.humanFirstReleaseDate)}',
-                        style: TextStyle(
-                          color: Colors.white70,
-                          fontSize: 18,
+                    ),
+                    Positioned(
+                      top: -5,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: 25,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        child: Center(
+                          child: Container(
+                            width: 40,
+                            height: 5,
+                            decoration: BoxDecoration(
+                              color: Colors.white54,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
                         ),
                       ),
-                      SizedBox(height: 16),
-                      if (game.storyline != null && game.storyline.isNotEmpty)
-                        Text(
-                          game.storyline,
-                          style: TextStyle(
-                            color: Colors.white70,
-                            fontSize: 16,
-                          ),
-                        )
-                      else
-                        Text(
-                          'No description available.',
-                          style: TextStyle(
-                            color: Colors.white70,
-                            fontSize: 16,
-                          ),
-                        ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               );
             },
