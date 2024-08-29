c84361902a3e32f83e98754afa69ffa320709b27
kevin <cosmoantenucci@gmail.com>
2024-08-29 11:47:47 +0200

    Updated game details page: - added an appbar - moved "favourite" and "add to library" buttons - added opacity fiilter to the background image

--- lib/ui/screens/game_details_page.dart
+++ lib/ui/screens/game_details_page.dart
@@ -12,128 +12,146 @@ class GameDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: [
-      game.coverUrl != null && game.coverUrl.isNotEmpty
-          ? Image.network(
-              game.coverUrl,
-              width: double.infinity,
-              height: double.infinity,
-              fit: BoxFit.cover,
-            )
-          : Image.asset(
-              'assets/placeholder.jpg',
-              width: double.infinity,
-              height: double.infinity,
-              fit: BoxFit.cover,
-            ),
-      Positioned.fill(
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
-          child: Container(
-            color:
-                Colors.black.withOpacity(0),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
-        ),
-      ),
-      SafeArea(
-        child: Row(
-          children: [
+          actions: [
             IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.white),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+              icon: Icon(Icons.share, color: Colors.white),
+              onPressed: () {},
             ),
-            IconButton(
-              icon: Icon(Icons.favorite_border_outlined, color: Colors.white),
-              onPressed: () {
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.playlist_add, color: Colors.white),
-              onPressed: () {
-                Fluttertoast.showToast(
-                  msg: "Game added to your library!",
-                  toastLength: Toast.LENGTH_LONG,
-                  gravity: ToastGravity.TOP,
-                  backgroundColor: Colors.white,
-                  textColor: Colors.black87,
-                  fontSize: 16.0,
-                );
-              },
-            )
           ],
         ),
-      ),
-      DraggableScrollableSheet(
-        initialChildSize: 0.33,
-        minChildSize: 0.33,
-        maxChildSize: 0.92,
-        builder: (BuildContext context, ScrollController scrollController) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.8),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
+        body: Stack(children: [
+          game.coverUrl != null && game.coverUrl.isNotEmpty
+              ? Image.network(
+                  game.coverUrl,
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : Image.asset(
+                  'assets/placeholder.jpg',
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 0.2, sigmaY: 0.2),
+              child: Container(
+                color: Colors.black.withOpacity(0.5),
               ),
             ),
-            padding: EdgeInsets.all(16.0),
-            child: SingleChildScrollView(
-              controller: scrollController,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Container(
-                      width: 40,
-                      height: 5,
-                      decoration: BoxDecoration(
-                        color: Colors.white54,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      margin: EdgeInsets.only(bottom: 16.0),
-                    ),
+          ),
+          DraggableScrollableSheet(
+            initialChildSize: 0.33,
+            minChildSize: 0.33,
+            maxChildSize: 0.94,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.8),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
                   ),
-                  Text(
-                    game.name,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Release Date: ${DateFormat.yMMMMd().format(game.humanFirstReleaseDate)}',
-                    style: TextStyle(
-                      color: Colors.white70,
-                      fontSize: 18,
-                    ),
-                  ),
-                  SizedBox(height: 16),
-                  if (game.storyline != null && game.storyline.isNotEmpty)
-                    Text(
-                      game.storyline,
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: 16,
+                ),
+                padding: EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  controller: scrollController,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Container(
+                          width: 40,
+                          height: 5,
+                          decoration: BoxDecoration(
+                            color: Colors.white54,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          margin: EdgeInsets.only(bottom: 16.0),
+                        ),
                       ),
-                    )
-                  else
-                    Text(
-                      'No description available.',
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: 16,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              game.name,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.favorite_border_outlined,
+                                    color: Colors.white),
+                                onPressed: () {
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.playlist_add,
+                                    color: Colors.white),
+                                onPressed: () {
+                                  Fluttertoast.showToast(
+                                    msg: "Game added to your library!",
+                                    toastLength: Toast.LENGTH_LONG,
+                                    gravity: ToastGravity.TOP,
+                                    backgroundColor: Colors.white,
+                                    textColor: Colors.black87,
+                                    fontSize: 16.0,
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                    ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-    ]));
+                      SizedBox(height: 8),
+                      Text(
+                        'Release Date: ${DateFormat.yMMMMd().format(game.humanFirstReleaseDate)}',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      if (game.storyline != null && game.storyline.isNotEmpty)
+                        Text(
+                          game.storyline,
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 16,
+                          ),
+                        )
+                      else
+                        Text(
+                          'No description available.',
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 16,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ]));
   }
 }
