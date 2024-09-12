61a18f2b6c38c004d21f8ca5f094b1d929eb4419
kevin <cosmoantenucci@gmail.com>
2024-09-12 23:15:37 +0200

    Added an icon to show that the catalog item is expandable

--- lib/ui/screens/explore_screen/catalog_item_widget.dart
+++ lib/ui/screens/explore_screen/catalog_item_widget.dart
@@ -56,42 +56,58 @@ class _CatalogItemState extends State<CatalogItem> {
                     image: DecorationImage(
                       image: widget.game.coverUrl.isNotEmpty
                           ? NetworkImage(widget.game.coverUrl)
-                          : AssetImage(
-                          'assets/placeholder.jpg') as ImageProvider,
+                          : AssetImage('assets/placeholder.jpg')
+                              as ImageProvider,
                       //TODO: gestire il caricamento
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
               ),
-              Positioned.fill(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.black.withOpacity(0.6),
-                        Colors.black.withOpacity(0.3),
-                      ],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ),
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black.withOpacity(0.6),
+                      Colors.black.withOpacity(0.3),
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
                   ),
                 ),
               ),
+            ),
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-              child: Text(
-                widget.game.name,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                child: Text(
+                  widget.game.name,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            Positioned(
+              right: 8,
+              top: _isExpanded ? 8 : 0,
+              child: Center(
+                child: IconButton(
                   color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
+                  onPressed: () {
+                    setState(() {
+                      _isExpanded = !_isExpanded;
+                    });
+                  },
+                  icon: _isExpanded
+                      ? Icon(Icons.keyboard_arrow_up)
+                      : Icon(Icons.keyboard_arrow_down),
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
