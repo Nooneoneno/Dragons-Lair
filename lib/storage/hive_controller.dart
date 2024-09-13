d811c26796dd3a5d55a4e7f090f9ed518460705e
kevin <cosmoantenucci@gmail.com>
2024-09-13 10:27:14 +0200

    Fixed a mismatch in Hive box name

--- lib/storage/hive_controller.dart
+++ lib/storage/hive_controller.dart
@@ -3,22 +3,22 @@ import 'package:hive/hive.dart';
 
 class HiveHelper {
   static Future<void> addGameToLibrary(VideoGamePartial game) async {
-    var box = await Hive.openBox('gameLibrary');
+    var box = await Hive.openBox('userLibrary');
     await box.put(game.id, game);
   }
 
   static Future<List<VideoGamePartial>> getGameLibrary() async {
-    var box = await Hive.openBox('gameLibrary');
+    var box = await Hive.openBox('userLibrary');
     return box.values.cast<VideoGamePartial>().toList();
   }
 
   static Future<void> removeGameFromLibrary(String gameId) async {
-    var box = await Hive.openBox('gameLibrary');
+    var box = await Hive.openBox('userLibrary');
     await box.delete(gameId);
   }
 
   static Future<bool> isGameInLibrary(String gameId) async {
-    var box = await Hive.openBox('gameLibrary');
+    var box = await Hive.openBox('userLibrary');
     return box.containsKey(gameId);
   }
 }
