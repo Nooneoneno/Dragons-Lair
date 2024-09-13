98437554cb930460faa8bcfb77a48472a93d86ff
kevin <cosmoantenucci@gmail.com>
2024-09-13 10:24:56 +0200

    Added hive plugin to handle the storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:DragOnPlay/ui/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'storage/videogame_partial.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize .env file
   await dotenv.load(fileName: ".env");
+
+  // Initialize Hive storage
+  await Hive.initFlutter();
+  Hive.registerAdapter(GameAdapter());
+  await Hive.openBox('userLibrary');
+
   runApp(MyApp());
 }
 
