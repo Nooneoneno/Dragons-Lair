1a04d7b301e7fa059c1c11ee25b5435cd6e7813e
kevin <cosmoantenucci@gmail.com>
2024-09-13 11:22:26 +0200

    Updated library UI

--- lib/ui/screens/library_screen.dart
+++ lib/ui/screens/library_screen.dart
@@ -29,27 +29,44 @@ class _UserLibraryScreenState extends State<UserLibraryScreen> {
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: _library.isEmpty
-          ? Center(child: Text('Your library is empty!'))
+          ? Center(
+              child: Text(
+              'Your library is empty!',
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+            ))
           : ListView.builder(
-        itemCount: _library.length,
-        itemBuilder: (context, index) {
-          final game = _library[index];
-          return ListTile(
-            title: Text(game.name,style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
-            subtitle: Text(game.releaseDate.toString(),style: TextStyle(color: Colors.white30),),
-            trailing: IconButton(
-              icon: Icon(Icons.delete, color: Colors.white,),
-              onPressed: () async {
-                await HiveController.removeGameFromLibrary(game.id);
-                _loadLibrary();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('${game.name} removed from your library!')),
+              itemCount: _library.length,
+              itemBuilder: (context, index) {
+                final game = _library[index];
+                return ListTile(
+                  title: Text(
+                    game.name,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                  subtitle: Text(
+                    game.releaseDate.toString(),
+                    style: TextStyle(color: Colors.white30),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete_forever_rounded,
+                      color: Colors.redAccent,
+                    ),
+                    onPressed: () async {
+                      await HiveController.removeGameFromLibrary(game.id);
+                      _loadLibrary();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text(
+                                '${game.name} removed from your library!')),
+                      );
+                    },
+                  ),
                 );
               },
             ),
-          );
-        },
-      ),
     );
   }
 }
